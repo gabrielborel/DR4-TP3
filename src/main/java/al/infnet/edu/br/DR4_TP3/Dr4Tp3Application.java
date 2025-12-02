bf09457f9b1c691458389303bfca8951185fe005
Gabriel Borel <biel_borel@hotmail.com>
2025-12-02 09:35:36 -0500

    Projeto Java iniciado para conter os códigos do DR4-TP3

---
+++ src/main/java/al/infnet/edu/br/DR4_TP3/Dr4Tp3Application.java
@@ -0,0 +1,13 @@
+package al.infnet.edu.br.DR4_TP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr4Tp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dr4Tp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/al/infnet/edu/br/DR4_TP3/Dr4Tp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package al.infnet.edu.br.DR4_TP3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dr4Tp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
