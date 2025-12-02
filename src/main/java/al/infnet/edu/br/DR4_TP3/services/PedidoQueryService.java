f8735267e3e6afc8bbc02b62c9d9422390fecfda
Gabriel Borel <biel_borel@hotmail.com>
2025-12-02 15:48:48 -0500

    10. Classe query service 2: Crie no serviço de consulta da questão 10 um método para obter o registro da entidade da questão 7 pelo ID.

--- src/main/java/al/infnet/edu/br/DR4_TP3/services/PedidoQueryService.java
+++ src/main/java/al/infnet/edu/br/DR4_TP3/services/PedidoQueryService.java
@@ -1,19 +1,24 @@
 package al.infnet.edu.br.DR4_TP3.services;
 
+import al.infnet.edu.br.DR4_TP3.entities.PedidoEntity;
 import al.infnet.edu.br.DR4_TP3.events.DomainEvent;
+import al.infnet.edu.br.DR4_TP3.repositories.PedidoRepository;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class PedidoQueryService {
 
     private final EventStore eventStore;
+    private final PedidoRepository pedidoRepository;
 
-    public PedidoQueryService(EventStore eventStore) {
+    public PedidoQueryService(EventStore eventStore, PedidoRepository pedidoRepository) {
         this.eventStore = eventStore;
+        this.pedidoRepository = pedidoRepository;
     }
 
     public List<? extends DomainEvent> buscarEventosPorPedidoId(UUID pedidoId) {
@@ -21,5 +26,9 @@ public class PedidoQueryService {
                 .asStream()
                 .map(event -> (DomainEvent) event.getPayload())
                 .toList();
+    }
+
+    public Optional<PedidoEntity> buscarPorId(UUID pedidoId) {
+        return pedidoRepository.findById(pedidoId);
     }
 }
